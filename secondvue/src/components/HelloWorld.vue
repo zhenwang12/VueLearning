b433e4fff3439cf2b0629d891a758b836a3f203a
王真 <zhen.wang12@outlook.com>
2024-06-18 09:40:31 +0800

    自定义命令

--- secondvue/src/components/HelloWorld.vue
+++ secondvue/src/components/HelloWorld.vue
@@ -6,6 +6,7 @@
             check out the
             <a href="#" rel="noopener" @click="showHide">vue-cli documentation</a>.
         </p>
+        <input placeholder="shuru" ref="inp" v-mingling2>
         <h3>Installed CLI Plugins</h3>
         <ul>
             <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank"
@@ -13,7 +14,7 @@
             <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank"
                    rel="noopener">eslint</a></li>
         </ul>
-        <h3>Essential Links</h3>
+        <h3 v-color="red">Essential Links</h3>
         <ul>
             <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
             <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
@@ -21,7 +22,7 @@
             <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
             <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
         </ul>
-        <h3>Ecosystem</h3>
+        <h3 v-color="green">Ecosystem</h3>
         <ul>
             <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
             <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
@@ -36,9 +37,15 @@
 <script>
 export default {
     name: 'HelloWorld',
+    data() {
+        return {
+            red: 'red',
+            green: 'green'
+        }
+    },
     props: {
         msg: String,
-        show: Boolean
+        show: Boolean,
     },
     methods: {
         showHide() {
@@ -48,6 +55,24 @@ export default {
     },
     mounted() {
         console.log(this.$refs.topMsg)
+    },
+//局部注册指令 指令名：配置项
+    directives: {
+        mingling2: {
+            inserted(el) {
+                el.focus()
+            }
+        },
+        color: {
+            //inserted 元素被插入到页面时触发
+            inserted(el, binding) {
+                el.style.color = binding.value
+            },
+            //updated 值修改时触发
+            update(el, binding) {
+                el.style.color = binding.value
+            }
+        }
     }
 }
 </script>
--- secondvue/src/main.js
+++ secondvue/src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+//全局注册自定义指令
+Vue.directive('mingling', {
+    inserted(el) {
+        el.focus()
+    }
+})
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
